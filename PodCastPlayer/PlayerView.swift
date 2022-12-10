510675d263e0179e1177ca69b1ae2f8aead4dce5
aaronsleepy <aaron@kmong.com>
2022-12-10 21:01:46 +0900

    refactoring: Extract subview - @Binding을 이용하여 데이터 바인딩

--- PodCastPlayer/PlayerView.swift
+++ PodCastPlayer/PlayerView.swift
@@ -20,21 +20,9 @@ struct PlayerView: View {
                 .font(.title3)
                 .foregroundColor(.gray)
             
-            Button {
-                self.isPlaying.toggle()
-            } label: {
-                Image(systemName: isPlaying ? "pause.circle" : "play.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 80)
-                    .foregroundColor(.primary)
-            }
+            PlayButton(isPlaying: $isPlaying)
             
-            Image(systemName: isPlaying ? "sun.max.fill" : "sun.max")
-                .resizable()
-                .renderingMode(.original)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80, height: 80)
+            PlayingStatusView(isPlaying: $isPlaying)
 
         }
     }
@@ -43,5 +31,33 @@ struct PlayerView: View {
 struct PlayerView_Previews: PreviewProvider {
     static var previews: some View {
         PlayerView(episode: Episode.list[0])
+    }
+}
+
+struct PlayButton: View {
+    @Binding var isPlaying: Bool
+    
+    var body: some View {
+        Button {
+            self.isPlaying.toggle()
+        } label: {
+            Image(systemName: isPlaying ? "pause.circle" : "play.circle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 80)
+                .foregroundColor(.primary)
+        }
+    }
+}
+
+struct PlayingStatusView: View {
+    @Binding var isPlaying: Bool
+    
+    var body: some View {
+        Image(systemName: isPlaying ? "sun.max.fill" : "sun.max")
+            .resizable()
+            .renderingMode(.original)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80, height: 80)
     }
 }
