6c99481327f447f323ae672f9b847a4ba26d4da6
aaronsleepy <aaron@kmong.com>
2022-12-10 20:34:18 +0900

    init: first commit

---
+++ PodCastPlayer/PodCastPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodCastPlayerApp.swift
+//  PodCastPlayer
+//
+//  Created by Aaron on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct PodCastPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
